660d0ea408efeeefe163abf9869e90931abff0a0
SCarinciRod <SCarinciRod@users.noreply.github.com>
2025-11-11 08:19:35 -0300

    chore: massive dead code cleanup - remove obsolete tests, debug scripts, temp docs (dev_tools/, scripts/, root, src/)

--- scripts/test_n1_reuse.py
+++
@@ -1,6 +0,0 @@
-from dou_snaptrack.ui.app import _plan_live_fetch_n1_options
-
-if __name__ == "__main__":
-    lst = _plan_live_fetch_n1_options('DO1', '12-09-2025')
-    print('[TEST N1] options count =', len(lst))
-    print('[TEST N1] first few =', lst[:5])
